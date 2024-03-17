12d557794b270ae0097602db4c254f5576b24d05
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-03-17 13:59:23 +0530

    third

--- common_model/mainservice.py
+++ common_model/mainservice.py
@@ -7,22 +7,16 @@ def getProduct():
     product = df['product_name'] 
     product_drop = product.dropna()
     productObj = product_drop.drop_duplicates(keep=False)
-    product_list = []
-    for prod in productObj:
-        product_list.append(prod)
-    
-    return product_list
+
+    return productObj
+
 
 def getProductUrl():
     df = pd.read_csv("flipkart_com-ecommerce_sample.csv")
     product_url = df["product_url"]
     product_drop = product_url.dropna()
     productUrlObj  = product_drop.drop_duplicates(keep=False)
-    product_url = []
-    for prod in productUrlObj:
-        product_url.append(prod)
-    
-    return product_url
+    return productUrlObj
 
 
 def getRetailPrice():
@@ -30,9 +24,5 @@ def getRetailPrice():
     retail_price = df["retail_price"]
     retail_price_drop = retail_price.dropna()
     retail_price_dropObj  = retail_price_drop.drop_duplicates(keep=False)
-    retail_price = []
-    for prod in retail_price_dropObj:
-        retail_price.append(prod)
-    
-    return retail_price
+    return retail_price_dropObj
 
--- main.py
+++ main.py
@@ -9,16 +9,14 @@ port = 5100
 @app.route('/')
 def index():
     get_product = getProduct()
-    get_product_url = getProductUrl()
-    get_retail_price = getRetailPrice()
-    context = {
-        'get_product': get_product,
-        'get_product_url': get_product_url,
-        'get_retail_price': get_retail_price
-    }
-    return render_template('index.html', data=context)
+    product = []
+    for prod in get_product:
+        product.append(prod)
+
+    return jsonify(product)
 
 
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=port)
+    app.secret_key = 'super secret key'
+    app.run(host="0.0.0.0", port=port)
